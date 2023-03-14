79610bfe57b75c5815a92a42827672aadf09c77c
Borja Quevedo <borjota@gmail.com>
2023-03-14 22:54:41 +0100

    Create a route to get an order

--- src/main/kotlin/dev/niltsiar/luckybackend/routes/orders.kt
+++ src/main/kotlin/dev/niltsiar/luckybackend/routes/orders.kt
@@ -4,6 +4,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
@@ -13,6 +14,10 @@ fun Application.orderRoutes() {
         route("/orders") {
             post {
                 call.respond(HttpStatusCode.Created)
+            }
+
+            get {
+                call.respond(HttpStatusCode.OK)
             }
         }
     }
